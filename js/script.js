2bf57d8825586d7a0cf3520a1922c248260855e4
Junior <129011987+JuniorCesarMarques@users.noreply.github.com>
2023-04-27 14:18:43 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -3,9 +3,15 @@ var menuLista = document.getElementById("menuLista")
 function clickMenu() {
     if(menuLista.style.display == "none") {
         menuLista.style.display = "block"
-    } else {
+    } else{
         menuLista.style.display = "none"
     }
 }
 
-
+function showMenu() {
+    if(window.innerWidth >= 768) {
+        menuLista.style.display = "block"
+    } else {
+        menuLista.style.display = "none"
+    }
+}
